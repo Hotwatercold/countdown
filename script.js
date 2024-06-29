1125fadc9addbbcb2c201f02cd49891899576e41
Hotwatercold <jessicashannonliem@gmail.com>
2024-06-29 12:19:43 +0800

    complete
    uh debug on set interval not double

--- script.js
+++ script.js
@@ -4,10 +4,7 @@ const boxes=document.querySelectorAll(".deadline-format h4")
 const monthname=["january","february","march","april","may","june","july","august","september","october","november","december"]
 const dayname=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
 //this is the current date
-let tempdate=new Date();
-let tempyear=tempdate.getFullYear();
-let tempmonth=tempdate.getMonth();
-let tempday=tempdate.getDate();
+
 
 
 // this is the deadline Date
@@ -16,11 +13,16 @@ const year=deadlinedate.getFullYear();
 const month=deadlinedate.getMonth();
 const day=deadlinedate.getDate();
 const weekday=deadlinedate.getDay();
-const time=deadlinedate.getTime();
+
 
 giveaway.textContent=`giveaway ends on ${dayname[weekday]}, ${day} ${monthname[month]} ${year}`
 
 function getremainingtime(){
+    let tempdate=new Date();
+    // let tempyear=tempdate.getFullYear();
+    // let tempmonth=tempdate.getMonth();
+    // let tempday=tempdate.getDate();
+    const time=deadlinedate.getTime();
     let temptime=tempdate.getTime();
     let difference=time-temptime;
     let oneday=24*60*60*1000;
@@ -30,7 +32,7 @@ function getremainingtime(){
     let remaininghour=Math.floor((difference % oneday)/onehour);
     let remainingminute=Math.floor((difference % onehour)/oneminute);
     let remainingsecond=Math.floor((difference % oneminute)/1000);
-    console.log(remainingday+","+remaininghour+","+remainingminute+","+remainingsecond);
+    console.log(difference);
     let values=[remainingday,remaininghour,remainingminute,remainingsecond];
     // function format(item){
     //     if(item<10)
@@ -39,7 +41,10 @@ function getremainingtime(){
         box.innerHTML=values[index];
     });
 
-    
+    if(difference<=0){
+        clearInterval(countdown);
+        deadline.innerHTML=`<h4 class="expired">sorry, this giveaway has expired!</h4>`;
+    }
 }
 let countdown=setInterval(getremainingtime,1000);
 getremainingtime();
